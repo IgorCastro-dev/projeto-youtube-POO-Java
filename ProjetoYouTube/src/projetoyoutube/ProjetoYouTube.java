c05f8fb933369f52b67902d61e574d5c11ef7aba
IgorCastro-dev <igorjunqueira@id.uff.br>
2021-03-04 19:46:22 -0300

    :package: criando a classe pessoa e espectador
    Criei a classe pessoa e a classe espectador do projeto youtube

--- ProjetoYouTube/src/projetoyoutube/ProjetoYouTube.java
+++ ProjetoYouTube/src/projetoyoutube/ProjetoYouTube.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projetoyoutube;
-
-/**
- *
- * @author manol
- */
 public class ProjetoYouTube {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        Espectador e = new Espectador("ihor",22,"M","juba");
+        System.out.println(e.toString());
+    }   
 }
--- ProjetoYouTube/src/projetoyoutube/Video.java
+++ ProjetoYouTube/src/projetoyoutube/Video.java
@@ -1,14 +1,81 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projetoyoutube;
+public class Video implements AcoesVideo{
+    private String titulo;
+    private int avaliacao;
+    private int views;
+    private int curtidas;
+    private boolean reproduzindo;
 
-/**
- *
- * @author manol
- */
-public class Video {
+    public Video(String titulo) {
+        this.titulo = titulo;
+        this.avaliacao = 1;
+        this.views = 0;
+        this.curtidas = 0;
+        this.reproduzindo = false;
+    }
+    
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(int avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public int getViews() {
+        return views;
+    }
+
+    public void setViews(int views) {
+        this.views = views;
+    }
+
+    public int getCurtidas() {
+        return curtidas;
+    }
+
+    public void setCurtidas(int curtidas) {
+        this.curtidas = curtidas;
+    }
+
+    public boolean isReproduzindo() {
+        return reproduzindo;
+    }
+
+    public void setReproduzindo(boolean reproduzindo) {
+        this.reproduzindo = reproduzindo;
+    }
+    
+    
+    
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public void pause() {
+        
+    }
+
+    @Override
+    public void like() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Video{" + "titulo=" + titulo + ", avaliacao=" + avaliacao + ", views=" + views + ", curtidas=" + curtidas + ", reproduzindo=" + reproduzindo + '}';
+    }
+    
+    
     
 }
